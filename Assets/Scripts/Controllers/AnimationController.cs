ce32dfae1ad1008360fbc22b94d57a2a7b5878ee
ardaerbaharli <ardaerbaharli1@gmail.com>
2021-08-02 01:55:14 +0300

    Changed how animations work. Now they don't need dummies to slide
    Removed layout groups from some of the objects. so they dont need a dummy now

--- Assets/Scripts/Controllers/AnimationController.cs
+++ Assets/Scripts/Controllers/AnimationController.cs
@@ -12,49 +12,38 @@ public class AnimationController : MonoBehaviour
         instance = this;
     }
 
-    public static IEnumerator SlideTo(List<GameObject> cards, Player toWho)
+    public static IEnumerator SlideToHand(List<GameObject> cardObjects, Player toWho)
     {
         Transform targetHand = toWho.playSide == PlaySide.Bottom ? GameControl.instance.bottomHand.transform : GameControl.instance.topHand.transform;
-        var parent = cards.First().transform.parent;
-        int childCount = cards.First().transform.parent.childCount;
+        var parent = cardObjects.First().transform.parent;
+        int childCount = parent.childCount;
 
-        var pos = GetPositions(targetHand, cards.Count);
-        for (int i = 0; i < pos.Count; i++)
+        foreach (var card in cardObjects)
         {
-            instance.StartCoroutine(Slide(cards[i], targetHand.transform, pos[i], GameSettings.slidingSpeed));
-            float rAngle;
-            if (toWho.playSide == PlaySide.Top) rAngle = 180f;
-            else rAngle = 0;
-            instance.StartCoroutine(FlipVertically(cards[i], GameSettings.slidingSpeed, rAngle));
+            instance.StartCoroutine(Slide(card, targetHand, GameSettings.slidingSpeed, false));
         }
 
         do
         {
             yield return null;
 
-        } while (parent.childCount != childCount - cards.Count);
+        } while (parent.childCount != childCount - cardObjects.Count);
+
+        LayoutRebuilder.ForceRebuildLayoutImmediate(parent.GetComponent<RectTransform>());
     }
 
-    public static IEnumerator SlideTo(GameObject card, GameObject target)
+    public static IEnumerator SlideToMiddle(GameObject card, Transform target)
     {
-        var parent = card.transform.parent;
-        var childCount = parent.childCount;
-
-        var pos = GetPositions(target.transform,1).First();
-        instance.StartCoroutine(Slide(card, target.transform, pos, GameSettings.slidingSpeed));
-
-        do
-        {
-            yield return null;
-
-        } while (parent.childCount != childCount - 1);
+        yield return instance.StartCoroutine(Slide(card, target, GameSettings.slidingSpeed));
     }
 
-
-    private static IEnumerator Slide(GameObject card, Transform targetParent, Vector3 position, float time)
+    private static IEnumerator Slide(GameObject card, Transform targetParent, float time, bool a = true)
     {
         if (!card.GetComponent<Card>().IsSliding)
         {
+            if (a)
+                card.transform.SetParent(targetParent);
+            var position = targetParent.transform.position;
             card.GetComponent<Card>().IsSliding = true;
 
             card.GetComponent<Canvas>().overrideSorting = true;
@@ -69,13 +58,11 @@ public class AnimationController : MonoBehaviour
                 yield return null;
             }
 
-
-            DestroyDummies();
-
-            card.transform.SetParent(targetParent);
-
             card.GetComponent<Canvas>().overrideSorting = false;
             card.GetComponent<Card>().IsSliding = false;
+            if(!a)
+                card.transform.SetParent(targetParent);
+
         }
     }
 
@@ -91,57 +78,6 @@ public class AnimationController : MonoBehaviour
             t += Time.deltaTime / seconds;
             card.GetComponent<RectTransform>().rotation = Quaternion.Lerp(card.GetComponent<RectTransform>().rotation, q, Mathf.SmoothStep(0f, 1f, t));
             yield return null;
-        }
-    }
-
-    public static Vector3 GetPosition(Transform targetParent)
-    {
-        var positionDummy = Instantiate(GameControl.instance.cardPrefab, targetParent) as GameObject;
-        positionDummy.AddComponent<Card>();
-        positionDummy.GetComponent<Card>().IsDummy = true;
-        positionDummy.tag = "dummy";
-        positionDummy.GetComponent<Image>().color = new Color(0, 0, 0, 0);
-
-        LayoutRebuilder.ForceRebuildLayoutImmediate(targetParent.GetComponent<RectTransform>());
-
-        var pos = positionDummy.transform.position;
-        Debug.Log(positionDummy.transform.parent.transform.position);
-        return pos;
-    }
-
-    public static List<Vector3> GetPositions(Transform targetParent, int count)
-    {
-        var posDummies = new List<GameObject>();
-
-        for (int i = 0; i < count; i++)
-        {
-            var positionDummy = Instantiate(GameControl.instance.cardPrefab, targetParent) as GameObject;
-            positionDummy.AddComponent<Card>();
-            positionDummy.GetComponent<Card>().IsDummy = true;
-            positionDummy.tag = "dummy";
-            positionDummy.GetComponent<Image>().color = new Color(0, 0, 0, 0);
-            posDummies.Add(positionDummy);
-        }
-
-        LayoutRebuilder.ForceRebuildLayoutImmediate(targetParent.parent.GetComponent<RectTransform>());
-        LayoutRebuilder.ForceRebuildLayoutImmediate(targetParent.GetComponent<RectTransform>());
-
-        var positions = new List<Vector3>();
-        foreach (var positionDummy in posDummies)
-        {
-            var pos = positionDummy.transform.position;
-            positions.Add(pos);
-        }
-        return positions;
-    }
-
-    private static void DestroyDummies()
-    {
-        var dummies = GameObject.FindGameObjectsWithTag("dummy");
-
-        foreach (var dummy in dummies)
-        {
-            DestroyImmediate(dummy);
         }
     }
 }
