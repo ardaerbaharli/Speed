67cb34f36dfbc85ca4a99643ba3be881c7e6e4e5
ardaerbaharli <ardaerbaharli1@gmail.com>
2021-08-06 05:58:46 +0300

    implemented aligncards function to set card positions depending on the card count

--- Assets/Scripts/Controllers/AnimationController.cs
+++ Assets/Scripts/Controllers/AnimationController.cs
@@ -14,35 +14,68 @@ public class AnimationController : MonoBehaviour
 
     public static IEnumerator SlideToHand(List<GameObject> cardObjects, Player toWho)
     {
-        Transform targetHand = toWho.playSide == PlaySide.Bottom ? GameControl.instance.bottomHand.transform : GameControl.instance.topHand.transform;
-        var parent = cardObjects.First().transform.parent;
-        int childCount = parent.childCount;
+        var targetHand = toWho.playSide == PlaySide.Bottom ? GameControl.instance.bottomHand.transform : GameControl.instance.topHand.transform;
 
         foreach (var card in cardObjects)
         {
-            instance.StartCoroutine(Slide(card, targetHand, GameSettings.slidingSpeed, false));
+            card.GetComponent<Canvas>().overrideSorting = true;
+            card.GetComponent<Canvas>().sortingOrder = -1;
+            card.transform.SetParent(targetHand);
         }
 
-        do
+        instance.StartCoroutine(AlignCards(targetHand));
+
+        yield return null;
+    }
+
+    public static IEnumerator AlignCards(Transform hand)
+    {
+        float leftBorderX = 0f;
+        float rightBorderX = Screen.width;
+
+        int cardCount = hand.transform.childCount;
+
+        var cardSize = new Vector3(hand.transform.GetChild(0).GetComponent<RectTransform>().rect.width, 0, 0);
+
+        var leftborder = new Vector3(leftBorderX, hand.transform.position.y, 0) + cardSize / 4;
+        var rightBorder = new Vector3(rightBorderX, hand.transform.position.y, 0) - cardSize / 4;
+
+        var length = rightBorder - leftborder;
+        var distanceBetweenCards = length / (cardCount + 1);
+
+        var pos = leftborder;
+        for (int i = 0; i < cardCount; i++)
         {
-            yield return null;
+            if (hand.transform.childCount < cardCount)
+                break;
 
-        } while (parent.childCount != childCount - cardObjects.Count);
+            pos += distanceBetweenCards;
+            var card = hand.transform.GetChild(i);
 
-        LayoutRebuilder.ForceRebuildLayoutImmediate(parent.GetComponent<RectTransform>());
+            float seconds = GameSettings.dealSlideTime;
+            float t = 0f;
+            while (t <= 1.0)
+            {
+                t += Time.deltaTime / seconds;
+                card.transform.position = Vector3.Lerp(card.transform.position, pos, Mathf.SmoothStep(0f, 1f, t));
+                yield return null;
+            }
+            card.GetComponent<Canvas>().overrideSorting = false;
+        }
     }
 
     public static IEnumerator SlideToMiddle(GameObject card, Transform target)
     {
-        yield return instance.StartCoroutine(Slide(card, target, GameSettings.slidingSpeed));
+        yield return instance.StartCoroutine(Slide(card, target, GameSettings.slowSlideTime));
     }
 
-    private static IEnumerator Slide(GameObject card, Transform targetParent, float time, bool a = true)
+    private static IEnumerator Slide(GameObject card, Transform targetParent, float time, bool setParent = true)
     {
         if (!card.GetComponent<Card>().IsSliding)
         {
-            if (a)
+            if (setParent)
                 card.transform.SetParent(targetParent);
+
             var position = targetParent.transform.position;
             card.GetComponent<Card>().IsSliding = true;
 
@@ -60,9 +93,9 @@ public class AnimationController : MonoBehaviour
 
             card.GetComponent<Canvas>().overrideSorting = false;
             card.GetComponent<Card>().IsSliding = false;
-            if(!a)
-                card.transform.SetParent(targetParent);
 
+            if (!setParent)
+                card.transform.SetParent(targetParent);
         }
     }
 
