3562b742209d2ec37fbe3164722ae8c7affef647
ardaerbaharli <ardaerbaharli1@gmail.com>
2021-08-14 01:30:47 +0300

    controls scene changes

---
+++ Assets/Scripts/Controllers/SceneController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    private static SceneController instance;
+    void Start()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+ 
+    public static void LoadGameScreen()
+    {
+        instance.StartCoroutine(LoadAsyncScene("Game"));
+    }
+
+    public static void LoadMainScreen()
+    {
+        instance.StartCoroutine(LoadAsyncScene("Main"));
+    }
+
+    public static IEnumerator LoadAsyncScene(string sceneName)
+    {
+        System.GC.Collect();
+        Resources.UnloadUnusedAssets();
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
