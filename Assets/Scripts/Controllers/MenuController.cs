a6d17387ded2c5fea9a9eaf7db6a7d39e4cd5c9b
ardaerbaharli <ardaerbaharli1@gmail.com>
2021-08-14 01:30:06 +0300

    main menu and ui items

---
+++ Assets/Scripts/Controllers/MenuController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] GameObject bottomScore;
+    [SerializeField] GameObject topScore;
+    void Start()
+    {
+        bottomScore.transform.GetChild(1).GetComponent<Text>().text = PlayerPrefs.GetInt("BottomPlayerScore", 0).ToString();
+        topScore.transform.GetChild(1).GetComponent<Text>().text = PlayerPrefs.GetInt("TopPlayerScore", 0).ToString();
+    }
+
+
+    public void StartGame()
+    {
+        SceneController.LoadGameScreen();
+    }
+
+    public void ResetScores()
+    {
+        PlayerPrefs.SetInt("TopPlayerScore", 0);
+        PlayerPrefs.SetInt("BottomPlayerScore", 0);
+        bottomScore.transform.GetChild(1).GetComponent<Text>().text = PlayerPrefs.GetInt("BottomPlayerScore", 0).ToString();
+        topScore.transform.GetChild(1).GetComponent<Text>().text = PlayerPrefs.GetInt("TopPlayerScore", 0).ToString();
+    }
+}
