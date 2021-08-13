56316f6916ba0923b2d62ac488350c2de7a5071b
ardaerbaharli <ardaerbaharli1@gmail.com>
2021-08-14 01:29:30 +0300

    falling symbols animation in the background

---
+++ Assets/Scripts/Controllers/BackgroundController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundController : MonoBehaviour
+{
+    [SerializeField] GameObject fallingSymbol;
+    private readonly string[] Suits = new string[] { "Spade", "Heart", "Club", "Diamond" };
+    private int interval;
+    void Start()
+    {
+        interval = GetInterval();
+    }
+
+    void Update()
+    {
+        if (Time.frameCount % interval == 0)
+        {
+            interval = GetInterval();
+            var obj = Instantiate(fallingSymbol, transform);
+            int index = Random.Range(0, 4);
+            obj.GetComponent<Image>().sprite = Resources.Load<Sprite>(Suits[index]);
+            obj.name = Suits[index];
+        }
+    }
+
+    private int GetInterval()
+    {
+        return Random.Range(200, 250);
+    }
+}
---
+++ Assets/Scripts/Objects/FallingSymbol.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class FallingSymbol : MonoBehaviour
+{
+    public float speed;
+    void Start()
+    {
+        transform.position = new Vector3(Random.Range(0, Screen.width), Screen.height + Screen.height / 20, 0);
+        speed = Random.Range(0.5f, 1.2f);
+    }
+
+    void Update()
+    {
+        var pos = transform.position;
+        var newPos = new Vector3(pos.x, pos.y - speed, 0);
+        transform.position = newPos;
+    }
+}
