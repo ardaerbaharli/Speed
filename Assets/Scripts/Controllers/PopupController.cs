0e92283d73016b6e87b29d5b4888153b7073de15
ardaerbaharli <ardaerbaharli1@gmail.com>
2021-08-28 03:07:18 +0300

    popup menu

---
+++ Assets/Scripts/Controllers/PopupController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupController : MonoBehaviour
+{
+    public GameObject background;
+    public GameObject tinyBackground;
+    public GameObject noButton;
+
+    private float fadeTime = 0.3f;
+
+    private void Start()
+    {
+        int index = Random.Range(0, 7);
+        tinyBackground.GetComponent<Image>().sprite = Resources.Load<Sprite>($"Backgrounds/BG{index}");
+
+        noButton.GetComponent<Button>().onClick.AddListener(delegate { NoButtonClick(); });
+        StartCoroutine(FadeInBackground());
+    }
+
+    public void NoButtonClick()
+    {
+        StartCoroutine(FadeOutBackground());
+        Destroy(gameObject, fadeTime - 0.1f);
+    }
+
+    private IEnumerator FadeInBackground()
+    {
+        float seconds = fadeTime;
+        float t = 0f;
+        Color32 targetColor = new Color32(36, 36, 36, 150);
+        while (t <= 1.0)
+        {
+            t += Time.deltaTime / seconds;
+            background.GetComponent<Image>().color = Color32.Lerp(background.GetComponent<Image>().color, targetColor, Mathf.SmoothStep(0f, 1f, t));
+            yield return null;
+        }
+    }
+    private IEnumerator FadeOutBackground()
+    {
+        float seconds = fadeTime;
+        float t = 0f;
+        Color32 targetColor = new Color32(36, 36, 36, 0);
+        while (t <= 1.0)
+        {
+            t += Time.deltaTime / seconds;
+            background.GetComponent<Image>().color = Color32.Lerp(background.GetComponent<Image>().color, targetColor, Mathf.SmoothStep(0f, 1f, t));
+            yield return null;
+        }
+    }
+}
