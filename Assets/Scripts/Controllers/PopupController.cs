a03a9c0c7279fff6402795a35ff045b20a9680e8
ardaerbaharli <ardaerbaharli1@gmail.com>
2021-09-24 11:13:55 +0300

    instead of using homemade animation controlls with my code, i switched to tweening library

--- Assets/Scripts/Controllers/PopupController.cs
+++ Assets/Scripts/Controllers/PopupController.cs
@@ -1,52 +1,33 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class PopupController : MonoBehaviour
 {
-    public GameObject background;
-    public GameObject tinyBackground;
-    public GameObject noButton;
+    [SerializeField] private GameObject background;
+    [SerializeField] private GameObject tinyBackground;
 
-    private float fadeTime = 0.3f;
+    private float fadeTime = 0.5f;
 
     private void Start()
     {
         int index = Random.Range(0, 7);
         tinyBackground.GetComponent<Image>().sprite = Resources.Load<Sprite>($"Backgrounds/BG{index}");
 
-        noButton.GetComponent<Button>().onClick.AddListener(delegate { NoButtonClick(); });
-        StartCoroutine(FadeInBackground());
+        background.GetComponent<Image>().DOFade(0.4f, fadeTime);
     }
 
-    public void NoButtonClick()
+    public void ResumeButton()
     {
-        StartCoroutine(FadeOutBackground());
-        Destroy(gameObject, fadeTime - 0.1f);
+        Time.timeScale = 1;
+        background.GetComponent<Image>().DOFade(0.1f, fadeTime);
+        tinyBackground.SetActive(false);
+        Destroy(gameObject, fadeTime);
     }
 
-    private IEnumerator FadeInBackground()
+    public void RestartButton()
     {
-        float seconds = fadeTime;
-        float t = 0f;
-        Color32 targetColor = new Color32(36, 36, 36, 150);
-        while (t <= 1.0)
-        {
-            t += Time.deltaTime / seconds;
-            background.GetComponent<Image>().color = Color32.Lerp(background.GetComponent<Image>().color, targetColor, Mathf.SmoothStep(0f, 1f, t));
-            yield return null;
-        }
-    }
-    private IEnumerator FadeOutBackground()
-    {
-        float seconds = fadeTime;
-        float t = 0f;
-        Color32 targetColor = new Color32(36, 36, 36, 0);
-        while (t <= 1.0)
-        {
-            t += Time.deltaTime / seconds;
-            background.GetComponent<Image>().color = Color32.Lerp(background.GetComponent<Image>().color, targetColor, Mathf.SmoothStep(0f, 1f, t));
-            yield return null;
-        }
+        SceneController.LoadGameScreen();
     }
 }
