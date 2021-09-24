60f2a90498fe4772fffdb6c75d42411ca09e1099
ardaerbaharli <ardaerbaharli1@gmail.com>
2021-09-24 11:13:23 +0300

    refactor and cleaning up

--- Assets/Scripts/Controllers/MenuController.cs
+++ Assets/Scripts/Controllers/MenuController.cs
@@ -3,8 +3,8 @@ using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
-    [SerializeField] GameObject bottomScore;
-    [SerializeField] GameObject topScore;
+    [SerializeField] private GameObject bottomScore;
+    [SerializeField] private GameObject topScore;
     void Start()
     {
         bottomScore.transform.GetChild(1).GetComponent<Text>().text = PlayerPrefs.GetInt("BottomPlayerScore", 0).ToString();
@@ -21,6 +21,7 @@ public class MenuController : MonoBehaviour
     {
         PlayerPrefs.SetInt("TopPlayerScore", 0);
         PlayerPrefs.SetInt("BottomPlayerScore", 0);
+
         bottomScore.transform.GetChild(1).GetComponent<Text>().text = PlayerPrefs.GetInt("BottomPlayerScore", 0).ToString();
         topScore.transform.GetChild(1).GetComponent<Text>().text = PlayerPrefs.GetInt("TopPlayerScore", 0).ToString();
     }
